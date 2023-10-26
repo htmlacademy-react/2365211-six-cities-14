f4ad68b658b966703ec9a25459bb95597057a936
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-10-26 18:16:34 +0300

    импорт стилей из page-not-found.css

--- src/pages/not-found-page/not-found-page.tsx
+++ src/pages/not-found-page/not-found-page.tsx
@@ -1,17 +1,17 @@
 import {Link} from 'react-router-dom';
 import {Helmet} from 'react-helmet-async';
-import styles from './page-not-found.css';
+//import styles from './page-not-found.css';
+import './page-not-found.css';
 
 export default function NotFoundPage(): JSX.Element {
   return (
-
-    <div className={`page ${styles.notfound}`}>
+    <div className={'notfound'}>
       <Helmet>
         <title>{'6 cities - Not Found'}</title>
       </Helmet>
-      <h1 className={styles.title}>404 NOT FOUND</h1>
-      <h3 className={styles.link}>
-        <Link to={'./'}>Go to main page</Link>
+      <h1 className={'title'}>404 NOT FOUND</h1>
+      <h3 className={'link'}>
+        <Link to={'/'}>Go to main page</Link>
       </h3>
     </div >
   );
