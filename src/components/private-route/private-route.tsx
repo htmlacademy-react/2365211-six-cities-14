03459cdb19ae1b827d21f8ccc5d4b019a6e4d3c2
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-11-04 14:42:01 +0300

    Исправлен путь в private-route

--- src/components/private-route/private-route.tsx
+++ src/components/private-route/private-route.tsx
@@ -1,13 +1,13 @@
 import {Navigate} from 'react-router-dom';
-import {AppRoute, AuthorizationStatus} from '../../const';
+import {AppRoute, AuthStatus} from '../../const';
 
 type PrivateRouteProps = {
   children: JSX.Element;
-  restrictedFor: AuthorizationStatus;
+  restrictedFor: AuthStatus;
   redirectTo: AppRoute;
 }
 
 export default function PrivateRoute({children, restrictedFor, redirectTo}:
    PrivateRouteProps): JSX.Element {
-  return restrictedFor === AuthorizationStatus.NoAuth ? <Navigate to={redirectTo} /> : children;
+  return restrictedFor === AuthStatus.NoAuth ? <Navigate to={redirectTo} /> : children;
 }
