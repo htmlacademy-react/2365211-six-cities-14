b62da8adff6a2aa100e8028a543c78e6bd8370f9
Коненкова Вера Николаевна <konenkovavera@yandex.ru>
2023-12-05 18:50:06 +0300

    Реализован компонент Sort

--- src/components/cities/cities.tsx
+++ src/components/cities/cities.tsx
@@ -1,8 +1,8 @@
 import { useState } from 'react';
 import CardList from '../card-list/card-list';
 import { CITIES_LOCATION } from '../../const';
-
 import Offer from '../../types/offer';
+import Sort from '../../components/sort/sort';
 import Map from '../map/map';
 
 type CitiesProps = {
@@ -20,21 +20,7 @@ function Cities({offers}: CitiesProps): JSX.Element {
         <section className="cities__places places">
           <h2 className="visually-hidden">Places</h2>
           <b className="places__found">{offers.length} places to stay in Amsterdam</b>
-          <form className="places__sorting" action="#" method="get">
-            <span className="places__sorting-caption">Sort by</span>
-            <span className="places__sorting-type" tabIndex={0}>
-              Popular
-              <svg className="places__sorting-arrow" width="7" height="4">
-                <use xlinkHref="#icon-arrow-select"></use>
-              </svg>
-            </span>
-            <ul className="places__options places__options--custom places__options--opened">
-              <li className="places__option places__option--active" tabIndex={0}>Popular</li>
-              <li className="places__option" tabIndex={0}>Price: low to high</li>
-              <li className="places__option" tabIndex={0}>Price: high to low</li>
-              <li className="places__option" tabIndex={0}>Top rated first</li>
-            </ul>
-          </form>
+          < Sort />
           <div className="cities__places-list places__list tabs__content">
             <CardList
               elementType={'cities'}
